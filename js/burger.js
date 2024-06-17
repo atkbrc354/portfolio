d312e53992d3ab8072f1f03e9a4961067fd1cb3f
Rinat <rinat.valeev.9@list.ru>
2024-06-17 11:44:06 +0300

    fix: burger and footer

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,22 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const headerList = document.querySelector('.header-list');
+const headerLink = document.querySelectorAll('.header-link');
+const body = document.querySelector('body');
+
+
+const nav = document.querySelector('nav');
 
 burgerMenu.addEventListener('click', () => {
   headerList.classList.toggle('show');
+  body.classList.toggle('hide-scroll');
 });
+
+
+
+body.addEventListener('click', function(event) {
+  if (event.target !== nav && event.target !== burgerMenu) {
+    headerList.classList.remove('show');
+    body.classList.remove('hide-scroll');
+  }
+});
+
